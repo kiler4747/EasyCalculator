f22225aa033b7c9000643fb722290c1038d6ec30
Artem-PC <kiler4747@gmail.com>
2013-05-16 17:55:20 +0300

    init commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calc
+{
+	public enum Angle
+	{
+		Deg,
+		Rad
+	}
+	public class Calculator
+	{
+		private string expression;
+		private int currentChar;
+		private Angle angle;
+
+		enum Funcs
+		{
+			Plus, Minus, Multiplicate, Devision,
+			Pow, Koren,
+			Sin, Cos, Tan,
+			Asin, Acos, Atan,
+			Ln,Lg,
+			Fact, Dot
+		}
+
+		Dictionary<Funcs, string> funcs = new Dictionary<Funcs, string>()
+			{
+				{Funcs.Plus, "+"},
+				{Funcs.Minus, "-"},
+				{Funcs.Multiplicate, "*"}
+			}; 
+
+		public string Expression
+		{
+			get { return expression; }
+			set
+			{
+				expression = value.Replace(" ", "");
+				expression = expression.Replace('.', ',');
+			}
+		}
+
+		public char CurrentChar
+		{
+			get { return Expression[currentChar]; }
+		}
+
+		public Calculator()
+		{
+			Expression = "";
+			currentChar = 0;
+		}
+
+		double SetAngle(double x)
+		{
+			if (angle == Angle.Deg)
+				return x*Math.PI/180;
+			return x;
+		}
+
+
+		public double Calculate(string str)
+		{
+			currentChar = 0;
+			Expression = str;
+			return Plus();
+		}
+
+		private double Plus()
+		{
+			double result = Multiplicate();
+			if (currentChar == Expression.Length)
+				return result;
+			while (Match("+") || Match("-"))
+			{
+				switch (CurrentChar)
+				{
+					case '+':
+						currentChar++;
+						result += Multiplicate();
+						break;
+					case '-':
+						currentChar++;
+						result -= Multiplicate();
+						break;
+				}
+			}
+			return result;
+		}
+
+		private bool Match(string patt)
+		{
+			if (patt.Length <= Expression.Length - currentChar)
+				if (patt.ToLower() == Expression.Substring(currentChar, patt.Length).ToLower())
+					return true;
+			return false;
+		}
+
+		private double Multiplicate()
+		{
+			double result = Pow();
+			if (currentChar == Expression.Length)
+				return result;
+			while (Match("*") || Match("/") || Match("√") ||
+					Match("(") || Match(")"))
+			{
+				switch (CurrentChar)
+				{
+					case '*':
+						currentChar++;
+						result *= Multiplicate();
+						break;
+					case '/':
+						currentChar++;
+						result /= Multiplicate();
+						break;
+					case '√':
+						result *= Pow();
+						break;
+					case '(':
+						result *= Brackets();
+						break;
+					case ')':
+						currentChar++;
+						break;
+				}
+			}
+			return result;
+		}
+
+		private double Pow()
+		{
+			double result = MathArifm();
+			if (currentChar == Expression.Length)
+				return result;
+			while (Match("^") || Match("√") || Match("!") || (Match("%")))
+			{
+				switch (CurrentChar)
+				{
+					case '^':
+						currentChar++;
+						result = Math.Pow(result, Pow());
+						break;
+					case '√':
+						currentChar++;
+						double step = 1;
+						if (result <= 1)
+						{
+							//Console.WriteLine("Степень корня должна быть больше 1");
+							return 0;
+						}
+						step /= result;
+						result = Pow();
+						result = Math.Pow(result, step);
+						break;
+					case '!':
+						currentChar++;
+						result = Factorial(result);
+						break;
+					case '%':
+						currentChar++;
+						result = result*0.01;
+						break;
+					default:
+						
+						break;
+				}
+			}
+			return result;
+		}
+
+		private double MathArifm()
+		{
+			double result = Brackets();
+			if (currentChar == Expression.Length)
+				return result;
+			while ( Match("sin") || Match("cos") ||
+					Match("tan") || Match("asin") ||
+					Match("acos") || Match("atan") ||
+					Match("ln") || Match("lg"))
+			{
+				switch (Expression.Substring(currentChar, 2))
+				{
+					case "ln":
+						currentChar += 2;
+						result = Math.Log(MathArifm());
+						break;
+					case "lg":
+						currentChar += 2;
+						result = Math.Log10(MathArifm());
+						break;
+					default:
+						switch (Expression.Substring(currentChar, 3))
+						{
+							case "sin":
+								currentChar += 3;
+								result = Math.Sin(SetAngle(MathArifm()));
+								break;
+							case "cos":
+								currentChar += 3;
+								result = Math.Cos(SetAngle(MathArifm()));
+								break;
+							case "tan":
+								currentChar += 3;
+								result = Math.Tan(SetAngle(MathArifm()));
+								break;
+							default:
+								switch (Expression.Substring(currentChar, 4))
+								{
+									case "asin":
+										currentChar += 3;
+										result = Math.Asin(SetAngle(MathArifm()));
+										break;
+									case "acos":
+										currentChar += 3;
+										result = Math.Acos(SetAngle(MathArifm()));
+										break;
+									case "atan":
+										currentChar += 3;
+										result = Math.Atan(SetAngle(MathArifm()));
+										break;
+								}
+								break;
+						}
+						break;
+				}
+			}
+			return result;
+		}
+
+		private double Factorial(double x)
+		{
+			if (x <= 1)
+				return 1;
+			return x*Factorial(x - 1);
+		}
+
+		private double Brackets()
+		{
+			double result = 0;
+			if (currentChar == Expression.Length)
+				return result;
+			while (Match("(") || Match(")"))
+			{
+				if (CurrentChar == '(')
+				{
+					currentChar++;
+					if (Expression.LastIndexOf(')') == -1 || Expression.LastIndexOf(')') < currentChar-1)
+					{
+						//Console.WriteLine("Нету закрывающей скобки");
+						return result;
+					}
+					Calculator calc = new Calculator();
+					result = calc.Calculate(Expression.Substring(currentChar, Expression.LastIndexOf(')') - currentChar));
+					currentChar = Expression.LastIndexOf(')') + 1;
+					return result;
+				}
+				if (CurrentChar == ')')
+				{
+					currentChar++;
+					//Console.WriteLine("Нету открывающей скобки");
+					return result;
+				}
+			}
+			return Digits();
+		}
+
+		private double Digits()
+		{
+			string number = "0";
+			while ((Expression.Length != currentChar) && (
+															(CurrentChar >= '0') &&
+															(CurrentChar <= '9') ||
+															(CurrentChar == '.') ||
+															CurrentChar == ','))
+			{
+				number += Expression[currentChar++];
+			}
+			return double.Parse(number);
+		}
+	}
+}
---
+++ EasyCalculator/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Calc;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EasyCalculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		Calculator calc = new Calculator();
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.  The Parameter
+		/// property is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			tbResult.Text = calc.Calculate(((TextBox)sender).Text).ToString("G");
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			int cursorPosition = tbExpression.SelectionStart;			
+			tbExpression.Text = tbExpression.Text.Insert(cursorPosition,((Button) sender).Content.ToString());
+			tbExpression.SelectionStart = cursorPosition + ((Button)sender).Content.ToString().Length;
+			tbExpression.Focus(FocusState.Keyboard);
+		}
+
+		private void btClear_Click(object sender, RoutedEventArgs e)
+		{
+			tbExpression.Text = string.Empty;
+			tbExpression.Focus(FocusState.Keyboard);
+		}
+
+		private void btCos_Click(object sender, RoutedEventArgs e)
+		{
+			int cursorPosition = tbExpression.SelectionStart;
+			tbExpression.Text = tbExpression.Text.Insert(cursorPosition,((Button)sender).Content.ToString() + "()");
+			tbExpression.SelectionStart = cursorPosition + (((Button) sender).Content.ToString() + "()").Length - 1;
+			tbExpression.Focus(FocusState.Keyboard);
+			//tbExpression.SelectionStart;
+		}
+
+	}
+}
